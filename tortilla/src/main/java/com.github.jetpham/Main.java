9b6f51ab3ed5c7b21df2e93b5cded3f5e7424aa6
Jet <jet_pham@student.davincischools.rg>
2020-10-23 16:14:20 -0700

    Changes. Archivation

--- tortilla/src/main/java/com.github.jetpham/Main.java
+++ tortilla/src/main/java/com.github.jetpham/Main.java
@@ -8,19 +8,16 @@ import org.javacord.api.DiscordApiBuilder;
 public class Main {
     public static void main(String[] args) {
         DiscordApi api = new DiscordApiBuilder()
-
-
-                .addServerBecomesAvailableListener(event -> {
-                    System.out.println("Loaded " + event.getServer().getName());
-                })
-                // A listener in their own class
-                .addListener(new MyListener())
-                // Alternative syntax that can be used for classes that require a DiscordApi parameter in their constructor
-                .setToken("NzYxNzIzNzAxNTY1NDU2NDQ0.X3eweg.viX6x6B9hteARPc7Omh8IVpKR2I")
-                .setWaitForServersOnStartup(false)
-                .login()
-                .join();
-
+            .addServerBecomesAvailableListener(event -> {
+                System.out.println("Loaded " + event.getServer().getName());
+            })
+            // A listener in their own class
+            .addListener(new MyListener())
+            // Alternative syntax that can be used for classes that require a DiscordApi parameter in their constructor
+            .setToken("NzYxNzIzNzAxNTY1NDU2NDQ0.X3eweg.viX6x6B9hteARPc7Omh8IVpKR2I")
+            .setWaitForServersOnStartup(false)
+            .login()
+            .join();
     }
 }
 
